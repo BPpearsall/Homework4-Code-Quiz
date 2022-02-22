8a92bf6d19112e5b3306e8636daa8caa8e098772
Lyere <BPpearsall@gmail.com>
2022-02-22 18:55:18 -0500

    added universal JS variables, cleaned up HTML

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let highscoreEl = $('#highscore');
+let timerTextEl = $('#timer-text');
+let timerCountEl = $('#timer-count');
+
+let startScreenEl = $('#start-screen');
+let questionScreenEl = $('#question-screen');
+let startBtnEl = $('#startBtn');
